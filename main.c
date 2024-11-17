feff3631a611c9535e458c58cf5adc4ea974618d
SH-Codes <wldrms@outlook.com>
2024-11-17 22:18:45 +0200

    simple shell fixed

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 #include "shell.h"
 
-/* Forward declaration of the function */
-void handle_open_error(char *prog_name, char *file_name);
-
 /**
  * main - entry point
  * @ac: arg count
@@ -13,42 +10,35 @@ void handle_open_error(char *prog_name, char *file_name);
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int fd = 5;
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
-			handle_open_error(av[0], av[1]);
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
 				exit(127);
+			}
 			return (EXIT_FAILURE);
 		}
 		info->readfd = fd;
 	}
-
 	populate_env_list(info);
 	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
-}
-
-/**
- * handle_open_error - handles errors related to file opening
- * @prog_name: name of the program (usually av[0])
- * @file_name: name of the file attempted to be opened
- */
-void handle_open_error(char *prog_name, char *file_name)
-{
-	if (errno == ENOENT)
-	{
-		_eputs(prog_name);
-		_eputs(": 0: Can't open ");
-		_eputs(file_name);
-		_eputchar('\n');
-		_eputchar(BUF_FLUSH);
-	}
 }
